c1c2085335d3b248af6943859ed7d8249c945f3e
williams-andrean <williams.andrean@yahoo.com>
2016-01-21 22:28:32 -0600

    begin work on blog page

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,3 @@ $(function() {
 	
 });
 
-$(document).on('click','.navbar-collapse.in',function(e) {
-
-    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
-        $(this).collapse('hide');
-    }
-
-});
